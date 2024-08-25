cc51fccc111eb97281714dd04cb7f52319c1095e
mhamadalkak22 <mhamadalkak220@gmail.com>
2024-08-24 21:26:20 -0700

    project complete

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { checkAuth, forgotPassword, login, logout, resetPassword, signup, verifyEmail } from "../controllers/auth.controllers.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+const router=express.Router();
+
+
+
+router.get('/check-auth',verifyToken,checkAuth)
+router.post("/signup", signup);
+router.post('/verify-email',verifyEmail)
+router.post('/logout',logout)
+router.post('/login',login)
+router.post('/forgot-password',forgotPassword)
+router.post('/reset-password/:token',resetPassword)
+
+
+
+export default router;
